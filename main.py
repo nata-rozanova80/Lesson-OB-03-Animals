9902d8167928a84dc4d5eea84ca76be47d52437c
nata-rozanova80 <nata.rozanova80@gmail.com>
2024-08-19 20:40:07 +0300

    add Bird mammal and reptile classes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # которые наследуют от класса `Animal`. Добавьте специфические атрибуты и
 # переопределите методы, если требуется (например, различный звук для `make_sound()`).
 
-class Animal():
+class Animal:
     def __int__(self, name, age):
         self.name = name
         self.age = age
@@ -16,3 +16,32 @@ class Animal():
     def eat(self):
         pass
 
+
+class Bird(Animal):
+    def make_sound(self):
+        print("Чирик-чирик")
+
+    def eat(self):
+        print("Птичка клюет")
+
+
+class Mammal(Animal):
+    def make_sound(self):
+        print("Му-му")
+
+    def eat(self):
+        print("Животное ест")
+
+
+class Reptile(Animal):
+    def make_sound(self):
+        print("ШШШШШ")
+
+    def eat(self):
+        print("Ням-ням")
+
+
+animals = [Bird(), Mammal(), Reptile()]
+for animal in animals:
+    animal.make_sound()
+    animal.eat()
